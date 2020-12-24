1430114aadefb17ea5834487cdd098c477b0bc65
Tamara Slosarek <tamaraslosarek@gmail.com>
2020-12-24 13:15:03 +0000

    Add Rivacold crawler

--- crawler.py
+++ crawler.py
@@ -1,17 +1,22 @@
 import argparse
 import time
 
-from constants import NORDCAP_OUTLET_CRAWLER, NORDCAP_DELIVERY_STATUS_CRAWLER
+from constants import NORDCAP_OUTLET_CRAWLER
+from constants import NORDCAP_DELIVERY_STATUS_CRAWLER
+from constants import RIVACOLD_DELIVERY_STATUS_CRAWLER
 from crawlers.nordcap_outlet import NordcapOutletCrawler
 from crawlers.nordcap_delivery_status import NordcapDeliveryStatusCrawler
+from crawlers.rivacold_delivery_status import RivacoldDeliveryStatusCrawler
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-no", "--{}".format(NORDCAP_OUTLET_CRAWLER), action="store_true")
 parser.add_argument("-nd", "--{}".format(NORDCAP_DELIVERY_STATUS_CRAWLER), action="store_true")
+parser.add_argument("-rd", "--{}".format(RIVACOLD_DELIVERY_STATUS_CRAWLER), action="store_true")
 
 crawlers = {
     NORDCAP_OUTLET_CRAWLER: NordcapOutletCrawler,
-    NORDCAP_DELIVERY_STATUS_CRAWLER: NordcapDeliveryStatusCrawler
+    NORDCAP_DELIVERY_STATUS_CRAWLER: NordcapDeliveryStatusCrawler,
+    RIVACOLD_DELIVERY_STATUS_CRAWLER: RivacoldDeliveryStatusCrawler
 }
 
 def main():
--- crawlers/rivacold_delivery_status.py
+++ crawlers/rivacold_delivery_status.py
@@ -1,50 +1,51 @@
 from crawlers.base import BaseCrawler
-from constants import RIVACOLD_DELIVERY_STATUS_CRAWLER, DELIVERY_STATUS_PROPERTIES
+from constants import RIVACOLD_DELIVERY_STATUS_CRAWLER
+from constants import DELIVERY_STATUS_PROPERTIES
+from constants import DELIVERY_STATUS_AVAILABLE
+from constants import DELIVERY_STATUS_SOON_AVAILABLE
+
+RIVACOLD_BASE_URL = "https://rivacold.de"
 
 class RivacoldDeliveryStatusCrawler(BaseCrawler):
     name = RIVACOLD_DELIVERY_STATUS_CRAWLER
     urls = [
-        "https://rivacold.de/de/Rivacold/artikel/Wandaggregat/1",
-        "https://rivacold.de/de/Rivacold/artikel/Deckenaggregat/1",
-        "https://rivacold.de/de/Rivacold/artikel/Splitaggregat/1"
+        "{}/de/Rivacold/artikel/Wandaggregat".format(RIVACOLD_BASE_URL),
+        "{}/de/Rivacold/artikel/Deckenaggregat".format(RIVACOLD_BASE_URL),
+        "{}/de/Rivacold/artikel/Splitaggregat".format(RIVACOLD_BASE_URL)
     ]
     header = DELIVERY_STATUS_PROPERTIES
 
-    def get_total_pages(self, page):
-        # indicator = page.find_all("span", "paging--display")[0]
-        # number = int(indicator.find_all("strong")[0].text)
-        # return(number)
-
     def get_page(self, base_url, page_number):
-        # ordering = "1" # Erscheinungsdatum
-        # items_per_page = "80"
-        # page_url = "{}?p={}&o={}&n={}".format(
-        #     base_url,
-        #     str(page_number),
-        #     ordering,
-        #     items_per_page
-        # )
-        # return self.get_soup(page_url)
+        page_url = "{}/{}".format(
+            base_url,
+            str(page_number)
+        )
+        return self.get_soup(page_url)
+
+    def get_total_pages(self, page):
+        indicator = page.find_all("a", "page-link")[-1]
+        number = int(indicator["href"].split("/")[-1])
+        return number
 
     def get_page_product_urls(self, page):
-        # product_titles = page.find_all("a", "product--title")
-        # page_product_urls = list(map(
-        #     lambda title: title["href"],
-        #     product_titles
-        # ))
-        # return(page_product_urls)
+        product_rows = page.find_all("div", "cilistitem")
+        page_product_urls = []
+        for product_row in product_rows:
+            product_title = product_row.find_all("h2")[0]
+            product_url = product_title.find_all("a")[0]["href"]
+            page_product_urls.append(RIVACOLD_BASE_URL + product_url)
+        return page_product_urls
 
     def get_product_information(self, product_page, product_url):
-        # article_number = product_page.find_all("span", "entry--content", itemprop = "sku")[0].text.strip()
-        # delivery_status_class = product_page.find_all("span", "delivery--text")[0]["class"][1]
-        # delivery_statuses = {
-        #     "delivery--text-available": 1,
-        #     "delivery--text-more-is-coming": 4,
-        #     "delivery--text-not-available": 5
-        # }
-        # if delivery_status_class in delivery_statuses:
-        #     delivery_status = delivery_statuses[delivery_status_class]
-        # else:
-        #     self.errors.append("Unbekannter Lieferstatus " + delivery_status_class + " in " + product_url)
-        #     delivery_status = None
-        # return [ article_number, delivery_status ]
+        article_number = product_page.find_all("h1")[0].text.strip()
+        article_number = article_number.split("-")[0]
+        article_number = article_number.split("/")[0]
+        article_number  = article_number[:10]
+        article_number = "RC-{}".format(article_number)
+        stock_number_text = product_page.find("div", { "id": "container2" }).find_all("p")[0].text
+        stock_number = int(stock_number_text.split(" ")[-1].strip())
+        if (stock_number > 0):
+            delivery_status = DELIVERY_STATUS_AVAILABLE
+        else:
+            delivery_status = DELIVERY_STATUS_SOON_AVAILABLE
+        return [ article_number, delivery_status ]
